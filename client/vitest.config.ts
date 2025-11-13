6565afecf95905eafc632d3eaf5bec8a76c5675d
Marc Stephenson <agentm@gmail.com>
2025-11-14 06:00:31 +0800

    vitest pool/fork memory leak issue

--- client/vitest.config.ts
+++ client/vitest.config.ts
@@ -2,11 +2,15 @@ import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()] as any,
   test: {
     environment: "jsdom",
     globals: true,
     setupFiles: ["./vitest.setup.ts"],
+    pool: "forks",
+    maxWorkers: 4,
+    fileParallelism: true,
+    isolate: true,
     coverage: {
       provider: "v8",
       reporter: ["text", "html"],
