89c40a252df009c6df04b48966ca2a5b58cfd2d8
Marc Stephenson <agentm@gmail.com>
2026-03-15 17:16:53 +0800

    pkg upg

--- client/vitest.config.ts
+++ client/vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()] as any,
+  plugins: [react()],
   test: {
     environment: "jsdom",
     globals: true,
